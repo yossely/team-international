dad103a7f05d1f9d4cacc7ad10b3eaf72b01fbf9
yossely <yossely7@gmail.com>
2018-11-05 02:14:02 -0500

    Display form errors

--- src/app/store/employees/employees.reducer.ts
+++ src/app/store/employees/employees.reducer.ts
@@ -1,12 +1,28 @@
 import { Employee } from '../../employees/shared/employee.model';
 import { EmployeeActions, EmployeeActionTypes } from './employees.actions';
+import { Country } from 'src/app/shared/models/country.model';
+import { AREA } from 'src/app/employees/shared/job.model';
 
 export interface EmployeesState {
   employees: { [id: string]: Employee };
 }
 
 export const initialState: EmployeesState = {
-  employees: {}
+  employees: {
+    'b1bfaaa0-e0b9-11e8-912f-190af83d9ebe': new Employee({
+      name: 'Woompy',
+      birthDate: new Date('2000-11-02T05:00:00.000Z'),
+      country: {
+        name: 'Albania',
+      } as Country,
+      username: 'DiegoJ',
+      hireDate: new Date('2018-11-06T05:00:00.000Z'),
+      status: true,
+      area: AREA.SERVICES,
+      jobTitle: 'Tuttofare',
+      id: 'b1bfaaa0-e0b9-11e8-912f-190af83d9ebe'
+    } as Employee)
+  }
 };
 
 export function employeesReducer(state = initialState, action: EmployeeActions): EmployeesState {
